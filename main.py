b0026799cb8f5f0ec5a011f670b845a9ef97d4ea
Daniel Green <dangreen07@outlook.com>
2024-03-02 12:02:36 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+class Drone:
+    locatiion = (0,0)
+    charge = 1 ## 100% charge
+    chargeLostPerMeter = 0.01 ## 1% loss
+    speed = 20 ## Meters per second
+    def __init__(self) -> None:
+        pass
