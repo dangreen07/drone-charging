6189d7d9ed970e10875fe6b217c91a77d6f49c68
Daniel Green <dangreen07@outlook.com>
2024-03-02 13:22:14 +0000

    Added Client

--- Client/index.js
+++ Client/index.js
@@ -6,22 +6,14 @@ const port = "8080";
 
 // Create a http server
 const server = http.createServer((req, res) => {
-    let filePath = __dirname + "/public/index.html";
-
     console.log(req.url);
-
-    switch(req.url) {
-        case '/':
-            filePath = __dirname + "/public/index.html";
-            break;
-        default:
-            filePath = __dirname + "/public" + req.url;
-            break;
+    let filePath = __dirname + "/public" + req.url;
+    if(req.url == "/") {
+        filePath = __dirname + "/public/index.html"
     }
 
     fs.readFile(filePath, (err, data) => {
         res.statusCode = 200;
-        res.setHeader("Content-Type","text/html");
         res.end(data, "utf-8");
     });
 })
